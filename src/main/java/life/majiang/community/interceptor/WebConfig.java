c2b84e5711f1e85c409b4bd38781c12bba4a46d9
starsea-community <975777829@qq.com>
2020-04-17 19:53:37 +0800

    图片保存路径

--- src/main/java/life/majiang/community/interceptor/WebConfig.java
+++ src/main/java/life/majiang/community/interceptor/WebConfig.java
@@ -1,6 +1,7 @@
 package life.majiang.community.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -13,14 +14,22 @@ public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private SessionInterceptor sessionInterceptor;
 
+    @Value("${location.upload.resource}")
+    private String resourceLocations;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //注册自定义拦截器，添加拦截路径和排除拦截路径
         registry.addInterceptor(sessionInterceptor).addPathPatterns("/**");
     }
 
+    /**
+     * 读取图片资源
+     *
+     * @param registry
+     */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/upload/**").addResourceLocations("file:\\IdeaProjects\\community\\src\\main\\resources\\static\\upload\\");
+        registry.addResourceHandler("/upload/**").addResourceLocations(resourceLocations);
     }
 }
