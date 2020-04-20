e6ebdf154868fcb5f25995a3010708889f91199a
starsea-community <975777829@qq.com>
2020-04-20 16:32:39 +0800

    图片保存路径

--- src/main/java/life/majiang/community/interceptor/WebConfig.java
+++ src/main/java/life/majiang/community/interceptor/WebConfig.java
@@ -30,6 +30,7 @@ public class WebConfig implements WebMvcConfigurer {
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        System.err.println(resourceLocations);
         registry.addResourceHandler("/upload/**").addResourceLocations(resourceLocations);
     }
 }
